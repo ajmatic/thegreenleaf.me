c149305d02bc6c73b3f065f7ef36bbca25c82004
Adam Lamagna <adamjames1920@gmail.com>
2014-02-02 14:02:33 -0500

    removed die statement

--- cms/db_connect.php
+++ cms/db_connect.php
@@ -1,6 +1,5 @@
 <?php
-var_dump($_SERVER);
-die; 
+ 
 if ("local.thegreenleaf.me" === $_SERVER["SERVER_NAME"]) {
 	$db = mysql_connect("localhost", "root", "Music2981") or die("Could not connect to databse.");
 	mysql_select_db("blog", $db);
